6e3322404adfb8cec3f5d3e24f6d394530fd0edd
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2024-02-15 10:52:30 -0500

    Updated file name

--- problem1.cpp
+++ problem1.cpp
@@ -14,16 +14,22 @@ class Labyrinth
 {
     private:
 
+
     public:
+    // Default constructor for this class.
     Labyrinth ()
     {
 
     }
 
+    // Sends one person into the labyrinth.
+    void enterLabyrinth(void)
+    {
 
 
 
 
+    }
 };
 
 
@@ -33,6 +39,11 @@ int main(void)
 {
     // Number of threads (people) to spawn.
     int n = 8;
+
+    Labyrinth myLab;
+
+
+    
 
 
 
@@ -48,3 +59,6 @@ int main(void)
 
 
 // --> every thread keeps track of how many people have entered the labyrinth?
+// when someone is called twice? they should leave the cake there?
+// or have everyone track the # of times another person has entered the maze.
+// 
